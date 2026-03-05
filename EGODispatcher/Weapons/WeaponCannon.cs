250eb95553b3295816f1f1c3e38f14cbab3ecb10
AnthonyWhite <1272448557@qq.com>
2026-03-05 09:58:56 +0800

    feature: 增加WeaponCannon

---
+++ EGODispatcher/Weapons/WeaponCannon.cs
@@ -0,0 +1,19 @@
+﻿using Utils;
+
+namespace EGODispatcher.Weapons
+{
+    class WeaponCannon : EquipmentScriptBase
+    {
+        public override bool OnGiveDamage(UnitModel actor, UnitModel target, ref DamageInfo dmg)
+        {
+            if (WeaponMethods.HasImmuneDefense(target)) {
+                dmg.type = (RwbpType)WeaponMethods.GetWeakestDefenseType(target);
+            }
+            return base.OnGiveDamage(actor, target, ref dmg);
+        }
+        public override void OnGiveDamageAfter(UnitModel actor, UnitModel target, DamageInfo dmg)
+        {
+            base.OnGiveDamageAfter(actor, target, dmg);
+        }
+    }
+}
